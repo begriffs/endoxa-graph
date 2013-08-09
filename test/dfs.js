15375f0f5b5e5e88a2296e1f56959aebf8b745e3
Joe Nelson <cred+github@begriffs.com>
2013-08-09 11:09:33 -0700

    More toposort tests

--- test/dfs.js
+++ test/dfs.js
@@ -36,6 +36,40 @@ describe('EndoxaGraph', function() {
         EndoxaGraph.fromConnectionsList([ [0, 1], [0, 2] ])
       )).to.eql([0,1,2]);
     });
+    it('orders a more complicated example', function() {
+      expect(EndoxaGraph.toposort(
+        EndoxaGraph.fromConnectionsList([
+          [0, 1], [0, 2],
+          [1, 3],
+          [2, 5],
+          [3, 4],
+          [4, 5], [4, 6],
+          [5, 6]
+        ])
+      )).to.eql([0,1,3,4,2,5,6]);
+      // the particular solution is not unique, but it is valid
+    });
+    it('orders another complicated example', function() {
+      expect(EndoxaGraph.toposort(
+        EndoxaGraph.fromConnectionsList([
+          [0, 1], [0, 2],
+          [1, 3],
+          [2, 1], [2, 4],
+          [3, 5],
+          [4, 5], [4, 6],
+          [5, 7],
+          [6, 7]
+        ])
+      )).to.eql([0,2,1,3,4,5,6,7]);
+      // the particular solution is not unique, but it is valid
+    });
+    it('refuses to sort a cycle', function() {
+      expect(function () {
+        EndoxaGraph.toposort(
+          EndoxaGraph.fromConnectionsList([ [0,1], [1, 0] ])
+        );
+      }).to.throw();
+    });
   });
 
 });
