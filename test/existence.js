83c387e998114b53929c1fbd51208c776865946c
Adam C. Baker <adam.durandal@gmail.com>
2013-09-04 21:37:35 -0700

    add some tests for fromConnectionsList

--- test/existence.js
+++ test/existence.js
@@ -1,9 +1,37 @@
-var graph = require('../module'),
-  chai       = require('chai'),
-  assert     = chai.assert;
+var graph  = require('../module')
+  chai     = require('chai')
+  assert   = chai.assert
+  claire   = require('claire')
+  data     = claire.data
+  PairList = data.Array(claire.sequence(data.Byte, data.Byte))
+  _        = require('lodash')
+;
 
 describe('EndoxaGraph', function() {
   it('exists', function() {
     assert.isObject(graph)
+  });
+
+  describe('fromConnectionsList', function(){
+    it('has as many edges as pairs passed',
+      claire.forAll(PairList).satisfy(function(pairs){
+        g = graph.fromConnectionsList(pairs);
+        assert.equal(g.nedges, pairs.length);
+        return true;
+      }).asTest()
+    );
+
+    it('has as many vertices as the max vertex passed',
+      claire.forAll(PairList).satisfy(function(pairs){
+        g = graph.fromConnectionsList(pairs);
+        if(pairs.length){
+          assert.equal(g.nvertices, _.max(_.flatten(pairs)));
+        }
+        else {
+          assert.equal(g.nvertices, 0);
+        }
+        return true;
+      }).asTest()
+    );
   });
 });
